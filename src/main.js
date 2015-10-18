82012302b15deb397289cc6c36b2db254594de3d
zxc0328 <120797887@qq.com>
2015-10-18 18:49:00 +0800

    update basic structure

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class React-markdown extends React.Component {
+    render() {
+        return <h1 > Hello World < /h1>
+    }
+}
+
+
+ReactDOM.render( < React-markdown / > , document.querySelector("#app"))
